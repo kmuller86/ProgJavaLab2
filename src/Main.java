1903f456fd09e61dc6c12bb9f37375dd09ba2f8b
Lukas <Lukasz90Balwierz@gmail.com>
2021-12-22 21:42:04 +0100

    Gotowe ćw.4

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,27 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        /* a */
+        for (int a = 0; a <100; a++) {
+            if (a % 5 == 0)
+                continue;
+            System.out.println(a);
+        }
+        System.out.println("-------------");
+        /* b */
+        for (int b = 0; b <=15; b++) {
+            if (b == 3)
+                break;
+            System.out.println(b);
+        }
+        System.out.println("-------------");
+        /* c */
+        for (int c = 0; c <70; c++) {
+            if (c == 62)
+                return;
+            System.out.println(c);
+        }
+
 
     }
 }
