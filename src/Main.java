ba3c7b2d52e836fd74156ab4541a91630688a9cf
Lukas <Lukasz90Balwierz@gmail.com>
2022-01-03 17:55:40 +0100

    Gotowe ćw.5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(metoda1(20)));
+
+        Metoda3();
+        Metoda3("Lukasz", "Balwierz");
+        Metoda3("Lukasz", "Balwierz", 24);
+        Metoda4(5,10);
+        Metoda4(3,2,2);
+
+
+
+
+
+    }
+
         /* ćwiczenie 5 Metody */
 
         /* zad.1
@@ -12,6 +26,19 @@ public class Main {
         c) napisać metodę, która wypisze otrzymane wartości,
         d) wstawić wywołanie metody do metody main
         */
+        public static int[] metoda1(int zmienna) {
+            int[] tab = new int[zmienna];
+            int a = 40;
+            for (int i = 0; i < zmienna; i++){
+                tab[i] = a;
+                --a;
+            }
+
+            return tab;
+        }
+
+
+
 
         /* zad.2
         a) stworzyc tablicę typu String[6],
@@ -20,14 +47,32 @@ public class Main {
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
 
+
+
+
+
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
             wyświetla w konsoli string który ma być waszym imieniem,
+
         b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
             wasze imie i nazwisko,
         c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
+
+        public static void Metoda3(){
+            System.out.println("Lukasz");
+        }
+
+        public static void Metoda3(String imie, String nazwisko) {
+            System.out.println("Imię to: " + imie + "," + " Nazwisko to: " + nazwisko);
+        }
+        public static void Metoda3(String imie, String nazwisko, int wiek) {
+            System.out.println("Imię to: " + imie + "," + " Nazwisko to: " + nazwisko + "," + " Wiek to: " + wiek);
+        }
+
+
 
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
@@ -37,7 +82,16 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
+        public static void Metoda4(int a, int b) {
+            int wynik = a + b;
+                System.out.println(wynik);
+        }
 
-    }
+        public static void Metoda4(int a, int b, int c) {
+            int wynik = a + b + c;
+            System.out.println(wynik);
+        }
+
+
 
 }
